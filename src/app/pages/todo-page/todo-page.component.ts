6aa1d6a1e416dc6fee0c8683e1eb9266ea85ab7d
alptekin-mert <mrtlptkn45@gmail.com>
2023-06-06 11:09:42 +0300

    lodash

--- src/app/pages/todo-page/todo-page.component.ts
+++ src/app/pages/todo-page/todo-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { Todo } from './models/todo';
-
+import * as _ from 'lodash'
 @Component({
   templateUrl: './todo-page.component.html',
   styleUrls: ['./todo-page.component.scss'],
 })
 export class TodoPageComponent {
+  todos: Todo[] = [];
+
   onItemAdd($event: Todo) {
     console.log('$event', $event);
+    const todo: Todo = {
+      id: this.todos.length + 1,
+      title: $event.title,
+      completed: $event.completed,
+      userId: $event.userId,
+    };
+    this.todos.push(todo);
   }
 }
